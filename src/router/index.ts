d2d1f35fd33f630b6b6b45329b929b3876ce2524
hyeon9782 <hyeon9782@gmail.com>
2024-03-25 08:53:07 +0900

    feat: router 동적 라우팅 설정

--- src/router/index.ts
+++ src/router/index.ts
@@ -8,6 +8,41 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('@/views/SignInView.vue')
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: () => import('@/views/SignUpView.vue')
+    },
+    {
+      path: '/settings',
+      name: 'settings',
+      component: () => import('@/views/SettingsView.vue')
+    },
+    {
+      path: '/editor',
+      name: 'create',
+      component: () => import('@/views/EditorView.vue')
+    },
+    {
+      path: '/editor/:slug',
+      name: 'editor',
+      component: () => import('@/views/EditorView.vue')
+    },
+    {
+      path: '/article/:slug',
+      name: 'article',
+      component: () => import('@/views/ArticleView.vue')
+    },
+    {
+      path: '/profile/:username',
+      name: 'profile',
+      component: () => import('@/views/ProfileView.vue')
     }
   ]
 })
