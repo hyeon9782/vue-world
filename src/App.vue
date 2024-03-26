846f2fe39da74dfc2f556c0fe589916017688098
hyeon9782 <hyeon9782@gmail.com>
2024-03-26 13:30:19 +0900

    design: HomeView 컴포넌트 마크업

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,19 @@
-<script setup lang="ts">
+<script lang="ts">
+import AppFooter from '@/components/layout/AppFooter.vue'
+import AppHeader from '@/components/layout/AppHeader.vue'
 import { RouterView } from 'vue-router'
+export default {
+  components: {
+    AppHeader,
+    AppFooter
+  }
+}
 </script>
 
 <template>
+  <AppHeader />
   <RouterView />
+  <AppFooter />
 </template>
 
 <style scoped></style>
