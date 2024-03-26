9cdeaa5def6e48527ccbc144eb6fd6f413b5a7d2
hyeon9782 <hyeon9782@gmail.com>
2024-03-26 13:29:52 +0900

    design: AppHeader, AppFooter 컴포넌트 마크업

--- src/components/layout/AppFooter.vue
+++ src/components/layout/AppFooter.vue
@@ -1,8 +1,21 @@
 <template>
-  <footer></footer>
+  <footer>
+    <div class="container">
+      <RouterLink to="/" class="logo-font">conduit</RouterLink>
+      <span class="attribution">
+        An interactive learning project from
+        <a href="https://thinkster.io">Thinkster</a>. Code &amp; design licensed under MIT.
+      </span>
+    </div>
+  </footer>
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue'
-export default defineComponent({})
+import { RouterLink } from 'vue-router'
+export default defineComponent({
+  components: {
+    RouterLink
+  }
+})
 </script>
 <style scoped></style>
--- src/components/layout/AppHeader.vue
+++ src/components/layout/AppHeader.vue
@@ -1,9 +1,36 @@
 <template>
   <header>
-    <nav>
-      <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/login">Sign in</RouterLink>
-      <RouterLink to="/register">Sign up</RouterLink>
+    <nav class="navbar navbar-light">
+      <div class="container">
+        <RouterLink to="/" class="navbar-brand">conduit</RouterLink>
+        <ul class="nav navbar-nav pull-xs-right">
+          <li class="nav-item">
+            <RouterLink to="/" class="nav-link">Home</RouterLink>
+          </li>
+          <li class="nav-item">
+            <RouterLink to="/login" class="nav-link">Sign in</RouterLink>
+          </li>
+          <li class="nav-item">
+            <RouterLink to="/register" class="nav-link">Sign up</RouterLink>
+          </li>
+          <li class="nav-item">
+            <RouterLink class="nav-link" to="/editor">
+              <i class="ion-compose"></i>&nbsp;New Article
+            </RouterLink>
+          </li>
+          <li class="nav-item">
+            <RouterLink class="nav-link" to="/settings">
+              <i class="ion-gear-a"></i>&nbsp;Settings
+            </RouterLink>
+          </li>
+          <li class="nav-item">
+            <RouterLink class="nav-link" to="/profile/eric-simons">
+              <img src="" class="user-pic" />
+              Eric Simons
+            </RouterLink>
+          </li>
+        </ul>
+      </div>
     </nav>
   </header>
 </template>
