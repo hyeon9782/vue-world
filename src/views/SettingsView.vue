a6d097327945f612976db1fba825990e21142c85
hyeon9782 <hyeon9782@gmail.com>
2024-03-30 23:59:03 +0900

    feat: setting page 데이터 패칭 및 사용자 정보 수정 기능 구현

--- src/views/SettingsView.vue
+++ src/views/SettingsView.vue
@@ -13,25 +13,28 @@
             <form>
               <fieldset>
                 <fieldset class="form-group">
-                  <VueInput type="text" placeholder="URL of profile picture" />
+                  <VueInput type="text" placeholder="URL of profile picture" v-model="user.image" />
                 </fieldset>
                 <fieldset class="form-group">
-                  <VueInput type="text" placeholder="Your Name" />
+                  <VueInput type="text" placeholder="Your Name" v-model="user.username" />
                 </fieldset>
                 <fieldset class="form-group">
                   <VueTextarea
                     class="form-control form-control-lg"
                     rows="8"
                     placeholder="Short bio about you"
+                    v-model="user.bio"
                   ></VueTextarea>
                 </fieldset>
                 <fieldset class="form-group">
-                  <VueInput type="text" placeholder="Email" />
+                  <VueInput type="text" placeholder="Email" v-model="user.email" />
                 </fieldset>
                 <fieldset class="form-group">
-                  <VueInput type="password" placeholder="New Password" />
+                  <VueInput type="password" placeholder="New Password" v-model="user.password" />
                 </fieldset>
-                <button class="btn btn-lg btn-primary pull-xs-right">Update Settings</button>
+                <button class="btn btn-lg btn-primary pull-xs-right" @click.prevent="changeUser">
+                  Update Settings
+                </button>
               </fieldset>
             </form>
             <hr />
@@ -46,10 +49,55 @@
 import { defineComponent } from 'vue'
 import VueInput from '@/components/common/VueInput.vue'
 import VueTextarea from '@/components/common/VueTextarea.vue'
+import { getUser, updateUser } from '@/api/user'
+import type { User } from 'types/user'
+import { mapActions } from 'pinia'
+import { useUserStore } from '@/stores/user'
 export default defineComponent({
   components: {
     VueInput,
     VueTextarea
+  },
+  data() {
+    return {
+      user: {} as User
+    }
+  },
+  methods: {
+    ...mapActions(useUserStore, ['updateUser']),
+
+    async fetchUserData() {
+      try {
+        const response = await getUser()
+        const user = response.data.user
+        this.user = user
+        console.log(user)
+      } catch (error) {
+        console.log(error)
+      }
+    },
+    async changeUser() {
+      try {
+        const response = await updateUser({
+          email: this.user.email,
+          username: this.user.username,
+          image: this.user.image,
+          bio: this.user.bio,
+          password: this.user.password
+        })
+        const user = response.data.user
+        console.log(user)
+        this.updateUser(user)
+        this.user = user
+        alert('변경 성공!')
+        this.$router.push('/')
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  },
+  created() {
+    this.fetchUserData()
   }
 })
 </script>
